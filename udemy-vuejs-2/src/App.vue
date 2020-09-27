41d0ec36057229bdaf4a500e43da7b1f20124f41
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-09-27 12:26:22 +0900

    単一ファイルコンポーネントのローカル登録

--- udemy-vuejs-2/src/App.vue
+++ udemy-vuejs-2/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <LikeNumber></LikeNumber>
+  <div>
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
 </template>
+
+<script>
+// LikeHeaderをローカル登録
+import LikeHeader from "./LikeHeader.vue";
+
+export default {
+  components: {
+    LikeHeader
+  }
+}
+</script>
