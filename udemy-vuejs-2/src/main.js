60b37b03df9adc19fd41b33266631e86b129a872
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-13 12:05:00 +0900

    ローカル登録

--- udemy-vuejs-2/src/main.js
+++ udemy-vuejs-2/src/main.js
@@ -4,17 +4,6 @@ import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive("border", (el, binding) => {
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if (binding.modifiers.round) {
-    el.style.borderRadius = ".5rem"
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, .26)";
-  }
-});
 
 new Vue({
   render: h => h(App),
