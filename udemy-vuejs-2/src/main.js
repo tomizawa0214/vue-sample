c6e71f444368dade92fb7df5639e032b9b07abc4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-13 11:31:08 +0900

    フック関数

--- udemy-vuejs-2/src/main.js
+++ udemy-vuejs-2/src/main.js
@@ -4,6 +4,24 @@ import LikeNumber from './components/LikeNumber.vue';
 
 Vue.config.productionTip = false;
 Vue.component('LikeNumber', LikeNumber);
+Vue.directive("border", {
+  // どのタイミングで処理をしたいかによって記述
+  bind(el, binding, vnode) {
+    // ディレクティブが初めて対象の要素に紐づいた時に一度限り
+  },
+  inserted(el, binding, vnode) {
+    // 親Nodeに挿入された時
+  },
+  update(el, binding, vnode, oldVnode) {
+    // コンポーネントが更新される度。子コンポーネントが更新される前
+  },
+  componentUpdated(el, binding, vnode, oldVnode) {
+    // コンポーネントが更新される度。子コンポーネントが更新された後
+  },
+  unbind(el, binding, vnode) {
+    // ディレクティブが紐づいている要素から取り除かれた時
+  }
+});
 
 new Vue({
   render: h => h(App),
