1a87d9eebf1038101fc2a952a05d7a543183ec13
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-15 00:28:49 +0900

    CSSトランジション

---
+++ udemy-vuejs-3/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* transitionのクラス */
+.fade-enter {
+  /* 現れる時の最初の状態 */
+  opacity: 0;
+}
+.fade-enter-active {
+  /* 現れる時のトランジションの状態 */
+  transition: opacity 2s;
+}
+.fade-enter-to {
+  /* 現れる時の最後の状態 */
+  opacity: 1;
+}
+.fade-leave {
+  /* 消える時の最初の状態 */
+  opacity: 1;
+}
+.fade-leave-active {
+  /* 消える時のトランジションの状態 */
+  transition: opacity 2s;
+}
+.fade-leave-to {
+  /* 消える時の最後の状態 */
+  opacity: 0;
+}
+
+.main {
+  width: 70%;
+  margin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
