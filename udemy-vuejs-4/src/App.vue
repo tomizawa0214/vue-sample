ce9bc54b0437c38a93a2690dfbf01f4fddf50985
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-15 13:27:52 +0900

    URLの切り替え

--- udemy-vuejs-4/src/App.vue
+++ udemy-vuejs-4/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link
+        to="/"
+        class="link"
+      >Home</router-link>
+      <router-link
+        to="/users"
+        class="link"
+      >Users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+</style>
