e862979ebbd002de5243071fb3b372b725e9eeb4
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-16 12:01:35 +0900

    すべてのページ遷移前の処理

--- udemy-vuejs-4/src/main.js
+++ udemy-vuejs-4/src/main.js
@@ -4,6 +4,14 @@ import router from "./router";
 
 Vue.config.productionTip = false
 
+// ページ遷移前の処理
+router.beforeEach((to, from, next) => {
+  if (to.path === "/users/1") {
+    next({ path: "/" });
+  }
+  next();
+});
+
 new Vue({
   router,
   render: h => h(App),
