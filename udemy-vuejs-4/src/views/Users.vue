145ae8fcfcf58267fe1b590c87aeeefa37eae573
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-27 00:45:38 +0900

    クエリの追加

--- udemy-vuejs-4/src/views/Users.vue
+++ udemy-vuejs-4/src/views/Users.vue
@@ -6,7 +6,7 @@
     <hr>
     <h1>User No. {{ id }}</h1>
     <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
-    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 } } ">次のユーザー</router-link>
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2 } } ">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
