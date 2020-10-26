4648f2ae227748c5e4301d595d6ebbb6dd3819a0
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-27 00:38:12 +0900

    動的なto属性

--- udemy-vuejs-4/src/views/Users.vue
+++ udemy-vuejs-4/src/views/Users.vue
@@ -5,6 +5,7 @@
     <router-link to="/users/2">ユーザー2</router-link>
     <hr>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
