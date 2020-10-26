1e7a2d22f72e208b35d5ccb75edb8a806d520904
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-27 01:03:11 +0900

    トランジション

--- udemy-vuejs-4/src/App.vue
+++ udemy-vuejs-4/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 50px;">
     <router-view name="header"></router-view>
-    <router-view></router-view>
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+</style>
