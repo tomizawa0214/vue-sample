a206a7bce832d810e70508baaf89af34de417e97
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-30 16:12:41 +0900

    Vuex state

--- udemy-vuejs-4/src/views/HeaderHome.vue
+++ udemy-vuejs-4/src/views/HeaderHome.vue
@@ -13,8 +13,23 @@
       exact
       class="link"
     >Users</router-link>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.state.count++
+    },
+    decrement() {
+      this.$store.state.count--
+    },
+  }
+}
+</script>
 
 <style scoped>
   .link {
--- udemy-vuejs-4/src/views/Home.vue
+++ udemy-vuejs-4/src/views/Home.vue
@@ -2,11 +2,17 @@
   <div>
     <h3>Home</h3>
     <button @click="toUsers">Usersのページに行く</button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
   methods: {
     toUsers() {
       this.$router.push({ path: "users/1/profile" });
