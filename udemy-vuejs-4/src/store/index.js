fdab4a4fb803f311a5e5a326ffc660ccae4eb769
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-10-31 16:21:11 +0900

    モジュール分割

--- udemy-vuejs-4/src/store/index.js
+++ udemy-vuejs-4/src/store/index.js
@@ -1,38 +1,27 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import count from './modules/count'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 2,
     message: ""
   },
   getters: {
-    doubleCount: state => state.count * 2,
-    tripleCount: state => state.count * 3,
     message: state => state.message
   },
   mutations: {
-    increment(state, number) {
-      state.count += number;
-    },
-    decrement(state, number) {
-      state.count -= number;
-    },
     updateMessage(state, newMessage) {
       state.message = newMessage;
     }
   },
   actions: {
-    increment({ commit }, number) {
-      commit("increment", number);
-    },
-    decrement({ commit }, number) {
-      commit("decrement", number);
-    },
     updateMessage({ commit }, newMessage) {
       commit("updateMessage", newMessage);
     }
+  },
+  modules: {
+    count
   }
 })
